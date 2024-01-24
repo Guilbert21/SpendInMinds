e270dc5d9698efd92226e76e7c7dd5692e04741f
Guilbert21 <guilbertalexis534@gmail.com>
2024-01-24 14:53:22 +0400

    Refactor App component and remove unused CSS files

--- spendInMinds/front-end/src/index.js
+++ spendInMinds/front-end/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './signUp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <signUp />
+    <>hello world</>
     <App />
   </React.StrictMode>
 );
