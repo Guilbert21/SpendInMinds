e71c7b24f9d97746ac919acf65f75b76b7a96083
Guilbert21 <guilbertalexis534@gmail.com>
2024-02-05 03:59:52 +0400

    Remove unused code and update dependencies

--- spendInMinds/front-end/src/App.js
+++ spendInMinds/front-end/src/App.js
@@ -1,19 +1,21 @@
 // app.jsx
-import { useState } from 'react';
-import SignUp from './signUp'; 
-import Login from './login';
-import Home from './home';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { useState } from 'react';
+// import SignUp from './signUp'; 
+// import Login from './login';
+// import Home from './home';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/register" element={<SignUp />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/home" element={<Home />} />
-            </Routes>
-        </BrowserRouter>
+        <div>
+            </div>
+        // <BrowserRouter>
+        //     <Routes>
+        //         {/* <Route path="/register" element={<SignUp />} />
+        //         <Route path="/login" element={<Login />} />
+        //         <Route path="/home" element={<Home />} /> */}
+        //     </Routes>
+        // </BrowserRouter>
     );
 }
 
